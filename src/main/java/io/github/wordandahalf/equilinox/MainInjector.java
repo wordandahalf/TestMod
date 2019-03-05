5b51a71507f34e047a858c4311db8ee7faeba3d6
Ryan J <mail2ryanjones@gmail.com>
2019-03-04 19:12:18 -0600

    Added more tests

--- src/main/java/io/github/wordandahalf/equilinox/MainInjector.java
+++ src/main/java/io/github/wordandahalf/equilinox/MainInjector.java
@@ -4,11 +4,12 @@ import equilinoxmodkit.util.EmkLogger;
 import io.github.wordandahalf.blueprint.annotations.At;
 import io.github.wordandahalf.blueprint.annotations.Blueprint;
 import io.github.wordandahalf.blueprint.annotations.Inject;
+import main.MainApp;
 
 @Blueprint(target = "main.MainApp")
 public class MainInjector {
-    @Inject(target = "main", at = @At(location = At.Location.HEAD))
+    @Inject(target = "main", at = @At(location = At.Location.TAIL))
     public static void main(String[] args) {
-        EmkLogger.log("Hello, from MainInjector!");
+        EmkLogger.log("Hello, from MainInjector with Equilinox version " + MainApp.VERSION_STRING);
     }
 }
